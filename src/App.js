9a15175d1583aded15255ad22182cb448b6f4d8f
Phoenix@Dibya <dibyaranjansahu.mail@gmail.com>
2022-06-15 20:31:30 +0530

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import Expenses from "./components/Expenses/Expenses";
 import "./App.css";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
-    const expense = [
-        { title: "Car Insurance", amount: "$255", date: new Date(2022, 2, 4) },
-        { title: "Bike Insurance", amount: "$155", date: new Date(2022, 3, 5) },
-        {
-            title: "Bycycle Insurance",
-            amount: "$55",
-            date: new Date(2022, 4, 6),
-        },
-        {
-            title: "Plastic Bike Insurance",
-            amount: "$5",
-            date: new Date(2022, 5, 7),
-        },
-    ];
+	const expense = [
+		{ title: "Car Insurance", amount: "$255", date: new Date(2022, 2, 4) },
+		{ title: "Bike Insurance", amount: "$155", date: new Date(2022, 3, 5) },
+		{
+			title: "Bycycle Insurance",
+			amount: "$55",
+			date: new Date(2022, 4, 6),
+		},
+		{
+			title: "Plastic Bike Insurance",
+			amount: "$5",
+			date: new Date(2022, 5, 7),
+		},
+	];
 
-    return (
-        <div>
-            <h3>Lets get started</h3>
-            <Expenses expense={expense} />
-        </div>
-    );
+	return (
+		<div>
+			<NewExpense />
+			<Expenses expense={expense} />
+		</div>
+	);
 }
 
 export default App;
